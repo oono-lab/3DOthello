04529b3ab11acd2a188c4b0910300564af264e2d
oono-lab <145289101+oono-lab@users.noreply.github.com>
2025-06-21 16:09:25 +0900

    Update AlphaValueChange.cs

--- Script/Setting/AlphaValueChange.cs
+++ Script/Setting/AlphaValueChange.cs
@@ -5,38 +5,62 @@ using TMPro;
 using UnityEngine;
 
 public class AlphaValueChange : MonoBehaviourPunCallbacks
-{   
-    public Material TargetObj;     
+{
+    #region Public Variables - Inspector参照用
+    // 透過値を変更する対象のマテリアル
+    public Material TargetObj;
+
+    // 現在の値を表示するテキスト
     public TextMeshProUGUI currentValueText;
+
+    // 値を調整するスクロールバー
     public UnityEngine.UI.Scrollbar scrollbar;
+
+    // 保存するPlayerPrefsのキー
     public string variableKey = "";
-    // Start is called before the first frame update
+    #endregion
+
+    #region Unityイベントメソッド処理
     void Start()
-    {   
+    {
+        // スクロールバーの値が変わったときのイベント登録
         scrollbar.onValueChanged.AddListener(delegate { onValueChangedAlpha(); });
     }
+
     void Update()
     {
+        // アルファ値をスクロールバーに応じて毎フレーム更新
         Color sourceColor = TargetObj.color;
         sourceColor.a = scrollbar.value;
+        TargetObj.color = sourceColor;  // 忘れずに更新！
+
+        // テキスト表示も更新
         currentValueText.text = scrollbar.value.ToString("F1");
     }
-    public void onValueChangedAlpha() {
-        
+    #endregion
+
+    #region アルファ変更時の保存処理
+    public void onValueChangedAlpha()
+    {
+        // プレイヤー設定としてアルファ値を保存
         PlayerPrefs.SetFloat(variableKey, scrollbar.value);
         PlayerPrefs.Save();
-        
     }
+    #endregion
 
-
+    #region オンライン同期処理
+    // 他のプレイヤーにマテリアル名を送信して同期させる
     public void OnlineColorMatch()
     {
         photonView.RPC("OnlineColorMatchPun", RpcTarget.Others, TargetObj.name);
     }
+
+    // RPCで受け取ったマテリアル名をもとに Resources から読み込み設定
     [PunRPC]
     private void OnlineColorMatchPun(string materialName)
     {
         Material found = Resources.Load<Material>("Materials/" + materialName);
         TargetObj = found;
     }
+    #endregion
 }
